46a42e6bcd68f8b1d0a0cf62c3f6ed23f7dd71f0
RaulORios <raul.orios89@outlook.com>
2021-09-29 09:41:54 -0300

    Arquivos atualizados

---
+++ src/Orientacao_Objeto_Nullable/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Orientacao_Objeto_Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nullable
+            
+            //Nullable<double> x = null;
+
+            /*
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine();
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            Console.WriteLine();
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("X is Null");
+            }
+
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is Null");
+            }
+            */
+
+            //Operador de coalescência nula
+
+            double? x = null;
+            double? y = 10;
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            
+            Console.ReadKey();
+        }
+    }
+}
